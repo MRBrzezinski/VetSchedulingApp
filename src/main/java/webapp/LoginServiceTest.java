ce6b953b126f395ae75cfa77da923734021316d3
kiwidoggie <forums@allenthinks.com>
2018-04-19 22:00:59 -0400

    Add JUnit for framework testing

---
+++ src/main/java/webapp/LoginServiceTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package webapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Allen Thomas
+ *
+ */
+class LoginServiceTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link webapp.LoginService#validateUser(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testValidateUser() {
+		fail("Not yet implemented");
+	}
+
+}
